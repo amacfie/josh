8f91a5acbda3e6d0922a09cf846f20e33eccc52a
Andrew MacFie <amacfie@sent.com>
2017-08-18 23:05:31 -0400

    Hide duplicate output from certain commands

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 var util = require('util');
 var colors = require('colors/safe');
 var shellUtil = require('./shell_util.js')
+var shell = require('shelljs');
+require('shelljs-plugin-clear');
+require('shelljs-plugin-sleep');
+
 
 var inspect = function(obj) {
   if (obj && typeof obj === 'object') {
@@ -19,9 +23,14 @@ var myWriter = function(output) {
       output.hasOwnProperty('stderr') &&
       !output.hasOwnProperty('__inspect'));
   if (isSS) {
-    var stderrPart = output.stderr || '';
-    var stdoutPart = output.stdout || '';
-    return colors.cyan(stderrPart + stdoutPart);
+    if (output['__hidden']) {
+      delete output['__hidden'];
+      return '';
+    } else {
+      var stderrPart = output.stderr || '';
+      var stdoutPart = output.stdout || '';
+      return colors.cyan(stderrPart + stdoutPart);
+    }
   } else {
     if (output && typeof output === 'object') {
       delete output['__inspect'];
@@ -38,9 +47,20 @@ var myrepl = require("repl").start({
   useColors: true,
   writer: myWriter});
 
-require('shelljs-plugin-clear');
-require('shelljs-plugin-sleep');
-var shell = require('shelljs');
+var hideWrap = function (fun, key) {
+  var outerRet = function() {
+    var ret = fun.apply(this, arguments);
+    if (ret instanceof Object) {
+      ret['__hidden'] = true;
+    }
+    return ret;
+  };
+  return outerRet;
+};
+
+['echo', 'exec'].forEach(function (key) {
+  shell[key] = hideWrap(shell[key], key);
+});
 Object.assign(myrepl.context, shell);
 myrepl.context['inspect'] = inspect;
 myrepl.context['shellUtil'] = shellUtil;
